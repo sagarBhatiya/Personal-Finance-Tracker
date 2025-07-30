8b1a9f45e59f8166b4fea629b45b6276e7d2006a
Sagar Bhatiya <sagarbhatiya12211@gmail.com>
2025-07-31 00:11:06 +0530

    Check

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://remarkable-kitsune-d472dd.netlify.app"],
   credentials: true,
 };
 app.use(cors(corsOption));
